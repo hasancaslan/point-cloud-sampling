10461a4d25738ab83a8174a546e8cdd456db73d9
Hasan Can <aslan4044@gmail.com>
2021-01-05 05:31:52 +0300

    feat: add voxelization code for real data

---
+++ data_voxel.py
@@ -0,0 +1,131 @@
+import open3d as o3d
+import numpy as np
+import matplotlib.pyplot as plt
+import copy
+import os
+import IPython.display
+import PIL.Image
+import random
+
+data_dir = "./test_data"
+
+
+class Cube(object):
+    def __init__(self, range):
+        """
+        Builds a cube from x, y and z ranges
+        """
+        self.range = range
+
+    """
+    @classmethod
+    def from_points(cls, firstcorner, secondcorner):
+        """ """
+        Builds a cube from the bounding points
+        Rectangle.from_points(Point(0, 10, -10),
+                              Point(10, 20, 0)) == Rectangle((0, 10), (10, 20), (-10, 0))
+        """ """
+    
+        x = (a[:, None] < b).all(-1)
+        return cls(*zip(firstcorner, secondcorner))
+    """
+
+    @classmethod
+    def from_voxel_size(cls, center, voxel_size):
+        """
+        Builds a cube from the voxel size and center of the voxel
+        """
+        cls.center = center
+        half_center = voxel_size / 2
+        x_range = (center[0] - half_center, center[0] + half_center)
+        y_range = (center[1] - half_center, center[1] + half_center)
+        z_range = (center[2] - half_center, center[2] + half_center)
+        range = np.array(
+            [[x_range[0], x_range[1], y_range[0], y_range[1], z_range[0], z_range[1]]]
+        )
+        return cls(range)
+
+    def contains_points(self, p):
+        """
+        Returns given point is in cube
+        """
+        less = np.repeat(self.range, repeats=[p.shape[0]], axis=0)[:, 0::2] < p
+        greater = np.repeat(self.range, repeats=[p.shape[0]], axis=0)[:, 1::2] > p
+        filter = np.logical_and(less.all(axis=1), greater.all(axis=1))
+        return p[filter]
+
+
+def get_random_color():
+    return [
+        round(random.uniform(0.0, 1.0), 1),
+        round(random.uniform(0.0, 1.0), 1),
+        round(random.uniform(0.0, 1.0), 1),
+    ]
+
+
+cube = Cube.from_voxel_size((5, 15, -5), 10)
+points = np.array([[3, 15, -8], [11, 15, -8]])
+
+cube.contains_points(points)
+
+if __name__ == "__main__":
+    data_dir = "./test_data"
+    N = 1000
+    voxel_size = 0.2
+    pcd_path = data_dir + "/cloud_bin_0.ply"
+    pcd = o3d.io.read_point_cloud(pcd_path)
+
+    print("N:", N, "voxel size:", voxel_size, "path:", pcd_path)
+
+    print("Downsampling...")
+    downpcd = pcd.voxel_down_sample(voxel_size=voxel_size)
+
+    pcdarray = np.asarray(pcd.points)
+    downarray = np.asarray(downpcd.points)
+
+    print("Drawing voxels...")
+    voxels = [Cube.from_voxel_size(center, voxel_size) for center in downarray]
+
+    print("Assigning voxels...")
+    pcds = []
+    for voxel in voxels:
+        pcd_voxel = o3d.geometry.PointCloud()
+        pcd_voxel.points = o3d.utility.Vector3dVector(voxel.contains_points(pcdarray))
+
+        pcds.append(pcd_voxel)
+
+    train_num = int(len(pcds) * 0.8)
+    test_num = int(len(pcds) * 0.1)
+
+    print(
+        "train set num:",
+        train_num,
+        "test set num:",
+        test_num,
+        "val set num:",
+        len(pcds) - train_num - test_num,
+    )
+    print("Writing...")
+
+    for i in range(len(pcds)):
+        if i < train_num:
+            batch = i // 30
+            num = i % 30
+            name = "train"
+        elif i < train_num + test_num:
+            batch = (i - train_num) // 30
+            num = (i - train_num) % 30
+            name = "test"
+        else:
+            batch = (i - train_num - test_num) // 30
+            num = (i - train_num - test_num) % 30
+            name = "val"
+
+        o3d.io.write_point_cloud(
+            f"./data/{name}/12_12_14@seq{str(batch).zfill(2)}_{str(num).zfill(3)}.ply",
+            pcds[i],
+        )
+
+        f = open(f"./data/{name}/12_12_14@seq{str(batch).zfill(2)}.txt", "a+")
+        f.write(f"12_12_14@seq{str(batch).zfill(2)}_{str(num).zfill(3)}.ply\n")
+        f.close()
